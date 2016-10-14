86ffb61ad2c7ed20425367b59ab7cd13af1a1ca0
Evan <daley.evan@gmail.com>
2016-10-14 08:30:56 -0700

    Add some of my prebuilt assets

---
+++ 2016-10-14/Assets/UtilityScripts/NavmeshPlayer/Targetting.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using Lean.Touch;
+
+public class Targetting : MonoBehaviour {
+
+	public LayerMask possibleTargetsMask;
+	public NavmeshPlayer player;
+	public WeaponControl weaponControl;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	protected virtual void OnEnable()
+	{
+		LeanTouch.OnFingerDown += HandleTouch;
+	}
+
+	protected virtual void OnDisable()
+	{
+		LeanTouch.OnFingerDown -= HandleTouch;
+	}
+
+	public void HandleTouch(LeanFinger finger)
+	{
+		if (LeanTouch.GuiInUse)
+		{
+			// the user touched a button, do nothing
+		} else
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (finger.ScreenPosition);
+			if (Physics.Raycast (ray, out hit, 100, possibleTargetsMask))
+			{
+				Attack (hit.point);
+			}
+		}
+	}
+
+	public void Attack(Vector3 target)
+	{
+		FaceTarget (target);
+		weaponControl.TryFireWeapons (target);
+	}
+
+	public void FaceTarget(Vector3 target)
+	{
+		target = new Vector3(target.x, transform.position.y, target.z);
+		transform.LookAt (target);
+	}
+}
---
+++ 2016-10-14/Assets/UtilityScripts/NavmeshPlayer/WeaponControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponControl : MonoBehaviour {
+
+	public GameObject rightHand;
+	public GameObject leftHand;
+	public Gun rightGun;
+	public Gun leftGun;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void EquipRight(Gun gun)
+	{
+		rightGun = gun;
+		gun.transform.SetParent (rightHand.transform);
+		gun.transform.position = rightHand.transform.position;
+		gun.transform.rotation = rightHand.transform.rotation;
+	}
+
+	public void EquipLeft(Gun gun)
+	{
+		leftGun = gun;
+		gun.transform.SetParent (leftHand.transform);
+		gun.transform.position = leftHand.transform.position;
+		gun.transform.rotation = leftHand.transform.rotation;
+	}
+
+	public void TryFireWeapons(Vector3 target)
+	{
+		if(rightGun != null)
+			rightGun.TryFire (target);
+		if(leftGun != null)
+			leftGun.TryFire (target);
+	}
+}
