44e799466d7735c4c2ea0f80d757262af5c8a057
Evan <daley.evan@gmail.com>
2016-10-14 08:56:44 -0700

    Create a new player health system

---
+++ 2016-10-14/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public float maxHealth;
+	public float maxShields;
+
+	private float curHealth;
+	private float curSheilds;
+
+	public float healthRegenRate;
+	public float shieldRegenRate;
+
+
+	void Start () 
+	{
+		curHealth = maxHealth;
+		curSheilds = maxShields;
+	}
+
+	public float Health
+	{
+		get{ return curHealth; }
+	}
+
+	public float HealthNormalized
+	{
+		get{ return curHealth / maxHealth; }
+	}
+
+	public float Shields
+	{
+		get{ return curSheilds; }
+	}
+
+	public float ShieldsNormalized
+	{
+		get{ return curSheilds / maxShields; }
+
+	}
+
+	void Update()
+	{
+		Regen ();
+		BindHealth ();
+	}
+
+	void BindHealth()
+	{
+		if (curHealth > maxHealth)
+			curHealth = maxHealth;
+
+		if (curSheilds > maxShields)
+			curSheilds = maxShields;
+
+		if (curHealth < 0)
+			curHealth = 0;
+
+		if (curSheilds < 0)
+			curSheilds = 0;
+	}
+
+	void Regen()
+	{
+		if(curHealth < maxHealth)
+			curHealth += Time.deltaTime * healthRegenRate;
+
+		if (curSheilds < maxShields)
+			curSheilds += Time.deltaTime * shieldRegenRate;
+	}
+
+}
---
+++ 2016-10-14/Assets/Scripts/SliderControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderControl : MonoBehaviour {
+
+	public SliderType sliderType;
+	public Slider slider;
+	public PlayerHealth playerHealth;
+
+	void Update () 
+	{
+		if (sliderType == SliderType.health)
+		{
+			slider.value = playerHealth.HealthNormalized;
+		}
+	}
+}
+
+public enum SliderType
+{
+	health,
+	shields,
+	mana
+}
