0f9a340c40d163aa9c1d399c5044c7740987d211
Evan <daley.evan@gmail.com>
2016-10-14 08:42:42 -0700

    Create basic scene

---
+++ 2016-10-14/Assets/Scripts/AutomaticGunPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutomaticGunPickup : MonoBehaviour 
+{
+	public WeaponControl weaponControl;
+	public GameObject gun;
+	public GameObject toDestroy;
+
+	void DetectObject()
+	{
+		GameObject weapon = GameObject.Instantiate (gun, transform.position, transform.rotation) as GameObject;
+		Gun gunScript = weapon.GetComponent<Gun> ();
+		weaponControl.EquipRight (gunScript);
+
+		Destroy (toDestroy);
+
+	}
+}
---
+++ 2016-10-14/Assets/Scripts/Gun.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public float fireRate = .2f;
+	float fireCooldown = 0;
+	public GameObject muzzle;
+	public float shotForce = 400f;
+	public GameObject bullet;
+
+	public void TryFire(Vector3 target)
+	{
+		if (Time.time > fireCooldown)
+		{
+			fireCooldown = Time.time + fireRate;
+			Fire (target);
+		}
+	}
+
+	public void Fire(Vector3 target)
+	{
+		if (bullet != null && muzzle != null)
+		{
+			Vector3 direction = target - muzzle.transform.position;
+			GameObject bulletInstance = GameObject.Instantiate (bullet, muzzle.transform.position, muzzle.transform.rotation) as GameObject;
+			Rigidbody rbody = bulletInstance.GetComponent<Rigidbody> ();
+			rbody.AddForce (direction.normalized * shotForce);
+		}
+
+	}
+}
+
